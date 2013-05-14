0dbf98c068c461c4845687816f1420754f42aaca
james wolpert <jameswolpert@gmail.com>
2013-05-14 13:33:35 -0700

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herpbook::Application.config.secret_token = '79fe86126cdeff9bd277408df205e11d7c3d498ea6d350ac316deb36c01dbfa7a4ef496437e791a5e799d274bd0e79e3d4964bdc28178cfb685d3a7352d36379'
